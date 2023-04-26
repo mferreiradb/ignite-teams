bd9183c9282d45e2cd3b159e9d27601a81606ffd
mferreiradb <mauhmg@gmail.com>
2023-04-26 20:48:27 -0300

    Added FlatLsit

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Hightlight';
 import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+
+	const [ groups, setGroups ] = useState<string[]>(['Turma 1', 'Turma 2', 'Turma 3']);
+
 	return (
 		<Styled.Container >
 			<Header />
 
 			<Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-			<GroupCard title='Turma teste' />
+			<FlatList
+				data={groups}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<GroupCard
+						key={item}
+						title={item} />
+				)}
+			/>
 		</Styled.Container>
 	);
 }
